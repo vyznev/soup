c62b683adc5c232da1579777ed362371d4af9b87
Ilmari Karonen <nospam@vyznev.net>
2016-08-25 15:30:22 +0300

    v1.45.16: don't spam console about getQuestionId() failures on non-question pages

--- SOUP.user.js
+++ SOUP.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/vyznev/
 // @description Miscellaneous client-side fixes for bugs on Stack Exchange sites (development)
 // @author      Ilmari Karonen
-// @version     1.45.15
+// @version     1.45.16
 // @copyright   2014-2016, Ilmari Karonen (http://stackapps.com/users/10283/ilmari-karonen)
 // @license     ISC; http://opensource.org/licenses/ISC
 // @match       *://*.stackexchange.com/*
@@ -1891,7 +1891,8 @@ var soupLateSetup = function () {
 	if ( window.StackExchange && StackExchange.ready ) StackExchange.ready( function () {
 		try {
 			var sid = StackExchange.options.site.id;
-			var qid = StackExchange.question.getQuestionId();
+			var qid = $('.question').data('questionid');
+			if ( !sid || !qid ) return;
 		
 			StackExchange.realtime.genericSubscribe( sid + '-question-' + qid, function ( json ) {
 				var data = $.parseJSON( json );
