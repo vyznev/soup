b6f362adddc7a519931b75176a0289c15c836952
Ilmari Karonen <nospam@vyznev.net>
2017-09-23 18:19:05 +0300

    v1.49.1: mse172931: avoid needless ajax queries on edit reviews

--- SOUP.user.js
+++ SOUP.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://github.com/vyznev/
 // @description Miscellaneous client-side fixes for bugs on Stack Exchange sites (development)
 // @author      Ilmari Karonen
-// @version     1.49.0
+// @version     1.49.1
 // @copyright   2014-2017, Ilmari Karonen (https://stackapps.com/users/10283/ilmari-karonen)
 // @license     ISC; https://opensource.org/licenses/ISC
 // @match       *://*.stackexchange.com/*
@@ -640,7 +640,7 @@ fixes.mse172931 = {
 	path:	/^\/review\b/,
 	script:	function () {
 		SOUP.hookAjax( /^\/review\/(next-task|task-reviewed)\b/, function () {
-			$('.reviewable-post').not(':has(.answer)').each( function () {
+			$('.reviewable-post').not(':has(.answer, .diffs)').each( function () {
 				var post = $(this), question = post.find('.question');
 				
 				// initial check to see if there are any answers to load
